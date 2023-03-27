4ae673a2cf4de3101040c9cb7c820067803c90c7
tanbirali <tanbirali2004@gmail.com>
2023-03-27 23:29:40 +0530

    Database was reset

--- Visitor/models.py
+++ Visitor/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 # Create your models here.
 class Visitor(models.Model):
-    name = models.CharField(max_length=50, null=True, blank=True)
+    name = models.CharField(max_length=50, default="Tanbir", null=False, blank=False)
--- Visitor/views.py
+++ Visitor/views.py
@@ -1,20 +1,20 @@
 from django.shortcuts import render
 
 from .models import Visitor
+
+
 # Create your views here.
 
 def index(request):
     if request.method == 'POST':
-        name = request.POST.get("Name")
+        name = request.POST.get("name")
+        print(name)
         visitor = Visitor(name=name)
         visitor.save()
-    return render(request,'index.html')
+    return render(request, 'index.html')
 
 
 def visitors(request):
-    visitors = Visitor.objects.all()
-    return render(request, 'visitors.html', {'visitors': visitors})
 
-
-
-
+    users = Visitor.objects.all()
+    return render(request, 'visitors.html', {'names': users})
